e3d779943d44039cb370a8a0faad15077c5fc5f4
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2023-04-10 05:19:34 +0300

    login auth fixed but cookies not savin

--- api/index.js
+++ api/index.js
@@ -3,16 +3,20 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  ///..other options
+};
 
-app.use(express.json());
+app.use(cors(corsOptions));
+
+app.use(cookieParser()); //middleware
+app.use(express.json()); //middleware
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
